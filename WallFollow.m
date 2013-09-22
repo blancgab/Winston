c0bfa8e1d13b9390d401f97d9c69504e2fec9467
Adam Reis <ahr2127@columbia.edu>
2013-09-22 17:01:03 -0400

    basic wall following algorithm that won't compile in simulator

--- WallFollow.m
+++ WallFollow.m
@@ -17,40 +17,73 @@ function runtime = WallFollow(serPort)
 
     % Variables
     t = tic;        % Time start
-    v = 0.3;        % Forward velocity (m/s)
+    v = 0.5;        % Forward velocity (m/s)
     w = 0;          % Angular velocity (rad/s)
+    p = 0.05;       % Pause time
     
+    forwardToMeetWall();
+    
+    followObjectClockwise();
+  
+end
+ 
+function forwardToMeetWall()
     % Start robot moving
     SetFwdVelAngVelCreate(serPort,v,w);
-
     while toc(t) < maxRuntime
-        
+        bumped = bumpCheck(serPort);
+        if bumped
+            return
+        end
+        pause(p)
+    end
+end
+
+function followObjectClockwise()
+    % Back up slightly   
+    v = -0.1;
+    w = 0;
+    SetFwdVelAngVelCreate(serPort,v,w);
+    pause(0.1);
+
+    % Turn left 45 degrees
+    v = 0;
+    w = -pi/2;
+    SetFwdVelAngVelCreate(serPort,v,w);
+    pause(1)
+    
+    % Turn forward to right with radius
+    v = 0.3;
+    r = 0.2;
+    SetFwdVelRadiusRoomba(serPort,v,r);
+    
+    while toc(t) < maxRuntime
         bumped = bumpCheck(serPort);
         
         if bumped
-            
+            % Back up slightly   
             v = -0.1;
             w = 0;
             SetFwdVelAngVelCreate(serPort,v,w);
-            BeepRoomba(serPort);
             pause(0.1);
             
+            % Turn left 45 degrees
             v = 0;
-            w = pi/2;
+            w = -pi/2;
             SetFwdVelAngVelCreate(serPort,v,w);
-            pause(2);
+            pause(1)
             
-            v= 0.3;
-            w = 0;
-            SetFwdVelAngVelCreate(serPort,v,w);
-            
+            % Turn forward to right with radius
+            v = 0.3;
+            r = 0.2;
+            SetFwdVelRadiusRoomba(serPort,v,r);
         end
-        pause(.05)
-        runtime = toc(t);
-%         output = 1;
+        pause(p)
     end
+    % output = 1;
 end
- 
+
+
 function bumped = bumpCheck(serPort)
 
 
