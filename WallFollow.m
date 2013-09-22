e9c9b5cce0c1960afb7642573785d78422b82ccb
Adam Reis <ahr2127@columbia.edu>
2013-09-22 15:42:04 -0400

    now the robot spins

--- WallFollow.m
+++ WallFollow.m
@@ -13,17 +13,23 @@ function output = WallFollow(serPort)
 % Gabriel Blanco and Adam Reis 2013
 
     % Variables
-    v = 10;               % Forward velocity (m/s)
+    v = 0.3;               % Forward velocity (m/s)
     w = 0;          % Angular velocity (rad/s)
     while 1
         bumped = bumpCheck(serPort);
         
         if bumped
+            SetFwdVelAngVelCreate(serPort,-0.1,w);
             BeepRoomba(serPort);
-            v= -1;
-            w= 0;
-            SetFwdVelAngVelCreate(serPort,v,w);
+            pause(0.1);
+            
+            SetFwdVelAngVelCreate(serPort,0,pi);
             pause(1);
+            v= 0.3;
+            w = 0;
+            SetFwdVelAngVelCreate(serPort,v,w);
+            pause(1)
+            SetFwdVelAngVelCreate(serPort,0,0);
             break;
         end
         
