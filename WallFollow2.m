ff1f1d1bc07d20cf8f8303f3bf99f2661e8f3bca
Adam Reis <ahr2127@columbia.edu>
2013-09-24 20:55:52 -0400

    shit works.

--- WallFollow2.m
+++ WallFollow2.m
@@ -15,90 +15,181 @@ function [ runtime ] = WallFollow2( serPort )
 % Global constants
 
     global maxRuntime port time speed radius step turnangle scan_freq ...
-        glob_theta rad2deg;
+        glob_theta rad2deg bumps;
     
     maxRuntime = 12000; 
     port = serPort;
     time = tic;
-    speed = 0.1;
+    speed = 0.07;
     radius = -0.3;
-    step = 0.05;
+    step = 0.07;
     turnangle = 120;
     scan_freq = 20;
     glob_theta = 90;
     rad2deg = 180/pi;
+    bumps = 0;
     
     followObjectClockwise();
+  
+%     v = 0;
+%     w = pi/2;
+%     AngleSensorRoomba(port);
+% %     t = tic;
+%     for n=1:5
+%         SetFwdVelAngVelCreate(port,v,w);
+%         pause(2);
+% %     while(glob_theta<(90+180*.96))
+% %         glob_theta = mod(glob_theta + AngleSensorRoomba(port)*rad2deg, 360);
+% %         fprintf('glob_theta: %f\n',glob_theta);
+% %         pause(step);
+% %     end
+% %     fprintf('angle sensor: %f\n',AngleSensorRoomba(port));
+%         SetFwdVelAngVelCreate(port,0,0);
+%         pause(1);
+%         dw = AngleSensorRoomba(port)*rad2deg;
+%         glob_theta = mod(glob_theta + AngleSensorRoomba(port)*rad2deg, 360);
+%         fprintf('dw: %f\n',dw);
+%         pause(1);
+%     end
+
+%     AngleSensorRoomba(port);
+%     turnAngle(port,1,180);
+%     pause(1);
+%     fprintf('angle sensor: %f\n',AngleSensorRoomba(port)*rad2deg);
+
+%     AngleSensorRoomba(port);
+%     SetFwdVelRadiusRoomba(port,speed,-1);
+%     
+%     while glob_theta>1
+%         glob_theta = mod(glob_theta+AngleSensorRoomba(port)*rad2deg, 360);
+%         fprintf('glob_theta: %f\n',glob_theta);
+%         pause(step);
+%     end
+%     SetFwdVelAngVelCreate(port,0,0);
+%     pause(1);
+%     glob_theta = mod(glob_theta+AngleSensorRoomba(port)*rad2deg, 360);
+%     fprintf('final glob_theta: %f\n',glob_theta);
+    
+
+
 end
 
 function followObjectClockwise()
 
     global maxRuntime port time radius speed step turnangle scan_freq ...
-        glob_theta rad2deg;
-
-    count = 0;
+        glob_theta rad2deg glob_x glob_y last_meas bumps start_x start_y calib;
     
-    v = 0.5;
     w = 0;
-    SetFwdVelAngVelCreate(port,v,w);
+    calib = 0;
+    glob_x = 0;
+    glob_y = 0;
+    AngleSensorRoomba(port);
+    last_meas = tic;
+    SetFwdVelAngVelCreate(port,speed,w);
         
-    while toc(time) < maxRuntime
-        
-       count = count +1; 
-  
+    while 1
        [front, left, right] = bumpCheck();
-               
-        if front
-            turn(turnangle);
-            SetFwdVelRadiusRoomba(port,speed,radius);
-            continue;
-        elseif left
-            turn(turnangle+40);
-            SetFwdVelRadiusRoomba(port,speed,radius);
-            continue;
-        elseif right
-            turn(turnangle-40);
-            SetFwdVelRadiusRoomba(port,speed,radius);
-            continue;
+        
+        if left || right || front
+            bumps = bumps+1;
+            fprintf('bumps: %d\n',bumps);
+            
+            if bumps==1
+                start_x = glob_x;
+                start_y = glob_y;
+                calib = -.0134;
+            end
+            
+%             backup();
+            
+            if front
+                turn(turnangle);
+                SetFwdVelRadiusRoomba(port,speed,radius);
+                continue;
+            elseif left
+                turn(turnangle+40);
+                SetFwdVelRadiusRoomba(port,speed,radius);
+                continue;
+            elseif right
+                turn(turnangle-40);
+                SetFwdVelRadiusRoomba(port,speed,radius);
+                continue;
+            end
         end
         
         calculatePosition();
         
+        dist_to_start = abs(pdist([glob_x,glob_y;start_x,start_y],'euclidean'));
+        %dist to start varies on time
+        fprintf('distance to starting point: %f\n',dist_to_start);
+        if bumps>4 && dist_to_start<.6
+            fprintf('CLOSE\n');
+            break;
+        end
+        
         pause(step);
 
     end
+    SetFwdVelAngVelCreate(port,0,0);
+    goToOrigin();
+end
+
+function backup()
+    global port speed glob_x glob_y glob_theta rad2deg;
+    SetFwdVelAngVelCreate(port,0,0);
+    backup_dist = -.005;
+%     glob_theta = mod(glob_theta + AngleSensorRoomba(port)*rad2deg,360);
+    travelDist(port, speed, backup_dist);
+%     
+%     glob_x = glob_x + cosd(glob_theta+180)*backup_dist;
+%     glob_y = glob_y + sind(glob_theta+180)*backup_dist;
+end
+
+function goToOrigin()
+    global glob_x glob_y glob_theta port speed bumps calib;
+    calib = 0;
+    if (glob_x>=0) %1st/4th quadrant
+        theta = mod(180+atand(glob_y/glob_x),360);
+        
+    elseif (glob_x<0) % 2nd/3rd
+        theta = atand(glob_y/glob_x);
+    end
+    
+    turn(theta-glob_theta);
+    
+    travelDist(port, speed, abs(pdist([glob_x,glob_y;0,0],'euclidean')));
+    
+    turn(90-glob_theta);
+    
+    fprintf('%f [0,0]\n',glob_theta);
+    fprintf('bumps: %d\n',bumps);
 end
 
 function calculatePosition()
 
-    global port step glob_theta rad2deg;
-
-    d_theta = AngleSensorRoomba(port)*rad2deg;
-    glob_theta = mod(glob_theta + d_theta,360);
-            
-    if d_theta
-        fprintf('Global Angle: %.2f\n',glob_theta);   
-    end
-
+    global port step glob_theta rad2deg glob_x glob_y speed last_meas calib;
+    dt = toc(last_meas);
+    last_meas = tic;
+    glob_x = glob_x + cosd(glob_theta)*speed*dt;
+    glob_y = glob_y + sind(glob_theta)*speed*dt;
+    
+    glob_theta = mod(glob_theta + AngleSensorRoomba(port)*rad2deg+calib,360); %.0134 is calibration
+           
+    fprintf('%f [%f,%f]\n',glob_theta, glob_x, glob_y);  
+    
+    
 end
 
 
 function turn(d)
-    global port glob_theta;
+    global port glob_theta rad2deg last_meas;
     
-    fprintf('Global Angle (before turn): %.2f\n',glob_theta);   
-    
-%     glob_theta = mod(glob_theta + d,360);
-%  
-%     fprintf('Global Angle (after turn):  %.2f\n',glob_theta);   
-    
-    v = 0;
-    w = pi/2;
-    SetFwdVelAngVelCreate(port,v,w);
-    pause(d/90);
-    
-    return;
+    turnAngle(port,.15,d);
+    glob_theta = mod(glob_theta + AngleSensorRoomba(port)*rad2deg,360);
+    last_meas = tic;
 end
+
+
 
 function [BumpFront, BumpLeft, BumpRight] = bumpCheck()
     global port;
