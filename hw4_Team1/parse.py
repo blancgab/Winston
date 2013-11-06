1767dfba644dec2da63df104620386f47e8995fa
Gabriel Blanco <gab2135@columbia.edu>
2013-11-06 01:27:19 -0500

    Expanded in parse.py

--- hw4_Team1/parse.py
+++ hw4_Team1/parse.py
@@ -2,12 +2,15 @@ __author__="Adam Reis <ahr2127@columbia.edu> and Gabriel Blanco <gab2135@columbi
 __date__ ="$Nov 5, 2013"
 
 import sys
+import pdb
+
+obstacles = []
 
 def parse_list(input_file):
 	input_file.seek(0,0)
 
 	num_obstacles = int(input_file.readline().strip())
-	obstacles = []
+	global obstacles
 
 	for j in range(num_obstacles):
 		num_vertices = int(input_file.readline().strip())
@@ -20,6 +23,36 @@ def parse_list(input_file):
 		obstacles.append(tmp)
 	return obstacles
 
+def expand_vertices(robot):
+	refl_robot = [(-x,-y) for x,y in robot]
+	print obstacles[4]
+
+	exp_obstacles = []
+
+	for obstacle in obstacles:
+
+		exp_tmp = []
+
+		for j in obstacle:
+
+			for i in refl_robot:
+
+				exp_x = float(j[0]+i[0])
+				exp_y = float(j[1]+i[1])
+
+				exp_tmp.append((exp_x,exp_y))
+
+		print "Obstacle: {}\n".format(obstacle)
+		print "Expanded: {}\n".format(exp_tmp)
+
+		exp_obstacles.append(exp_tmp)
+
+
+
 if __name__ == "__main__":
 	with open(sys.argv[1]) as input_file:
 		parse_list(input_file)	
+
+		robot = [(0,0),(0,-0.34),(-0.34,-0.34),(-0.34,0)]
+
+		expand_vertices(robot)
