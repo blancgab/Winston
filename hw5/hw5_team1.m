f442b7487eba0061f01486841d95069f6559f335
Gabriel Blanco <gab2135@columbia.edu>
2013-12-07 16:22:30 -0500

    hw5 read from cam

--- hw5/hw5_team1.m
+++ hw5/hw5_team1.m
@@ -1,8 +1,14 @@
 function hw5()
 
     % Reading your image
+    image = imread('http://192.168.1.100/snapshot.cgi?user=admin&pwd=&resolution=32&rate=0');
+    
+    resolution = size(img); 
+	resolution = resolution(1:2) 
 
-    img2 = imread('http://192.168.1.100/snapshot.cgi'); 
+    figure(1);
+    imshow(image);
 
+    
 end
 
