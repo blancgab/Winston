bdc58658c71680c57b45778aa2d4b4429cddb877
Adam Reis <adamhreis@gmail.com>
2013-12-08 00:50:58 -0500

    modified color detection algorithm and masking, so it works pretty well now

--- hw5/hw5_team1.m
+++ hw5/hw5_team1.m
@@ -3,7 +3,7 @@ function hw5()
     global image;
 
     % Reading your image
-    image = imread('http://192.168.1.100/snapshot.cgi?user=admin&pwd=&resolution=32&rate=0');
+    image = imread('http://192.168.1.100/snapshot.cgi?user=admin&pwd=&resolution=16&rate=0');
     
     resolution = size(image); 
 	resolution = resolution(1:2);
@@ -11,45 +11,22 @@ function hw5()
     figure(1);
     imshow(image);
     
-    P = getpts(1);
+    [c, r] = getpts(1);
     
-    sumr = 0;
-    sumb = 0;
-    sumg = 0;
-    
-    for i = P
-       
-        C = getColor(uint64(i(1)), uint64(i(2)), image);
-        
-        C(1)
-        C(2)
-        C(3)
-        
-        sumr = sumr + C(1);
-        sumg = sumg + C(2);
-        sumb = sumb + C(3);
-        
-    end
-    
-    sumr;
-    sumg;
-    sumb;
-    
-    r = sumr/length(P)
-    g = sumg/length(P)
-    b = sumb/length(P)
-    
+    rgb_values = impixel(image, c, r);
+
+    r = sum(rgb_values(:,1))/length(rgb_values)
+    g = sum(rgb_values(:,2))/length(rgb_values)
+    b = sum(rgb_values(:,3))/length(rgb_values)
     
     % Color Threshold
-    ct = 75;
-    
-    
+    ct = 25;
     
     pixel_mask = (r-ct) < image (:,:,1) & image(:,:,1) < (r+ct) & ...
                  (g-ct) < image (:,:,2) & image(:,:,2) < (g+ct) & ...
                  (b-ct) < image (:,:,3) & image(:,:,3) < (b+ct);
 
-
+    figure(2);
     imshow(pixel_mask);
 
     
