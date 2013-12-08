46e43de4c60a3b6f86b392896f99050fef40636d
Adam Reis <adamhreis@gmail.com>
2013-12-08 01:02:35 -0500

    detect threshold via min and max values, rather than average

--- hw5/hw5_team1.m
+++ hw5/hw5_team1.m
@@ -13,18 +13,35 @@ function hw5()
     
     [c, r] = getpts(1);
     
-    rgb_values = impixel(image, c, r);
+    rgb = impixel(image, c, r);
 
-    r = sum(rgb_values(:,1))/length(rgb_values)
-    g = sum(rgb_values(:,2))/length(rgb_values)
-    b = sum(rgb_values(:,3))/length(rgb_values)
+%     r = sum(rgb(:,1))/length(rgb)
+%     g = sum(rgb(:,2))/length(rgb)
+%     b = sum(rgb(:,3))/length(rgb)
+%     
+%     % Color Threshold
+%     ct = 25;
+%     
+%     pixel_mask = (r-ct) < image (:,:,1) & image(:,:,1) < (r+ct) & ...
+%                  (g-ct) < image (:,:,2) & image(:,:,2) < (g+ct) & ...
+%                  (b-ct) < image (:,:,3) & image(:,:,3) < (b+ct);
+
+    max_r = max(rgb(:,1))
+    max_g = max(rgb(:,2))
+    max_b = max(rgb(:,3))
+    
+    min_r = min(rgb(:,1))
+    min_g = min(rgb(:,2))
+    min_b = min(rgb(:,3))
     
     % Color Threshold
-    ct = 25;
+    ct = 5;
     
-    pixel_mask = (r-ct) < image (:,:,1) & image(:,:,1) < (r+ct) & ...
-                 (g-ct) < image (:,:,2) & image(:,:,2) < (g+ct) & ...
-                 (b-ct) < image (:,:,3) & image(:,:,3) < (b+ct);
+    pixel_mask = min_r-ct < image (:,:,1) & image(:,:,1) < max_r+ct & ...
+                 min_g-ct < image (:,:,2) & image(:,:,2) < max_g+ct & ...
+                 min_b-ct < image (:,:,3) & image(:,:,3) < max_b+ct;
+
+
 
     figure(2);
     imshow(pixel_mask);
