c85016e7b938768b5c49748ba77de199c6fc2afb
Adam Reis <adamhreis@gmail.com>
2013-12-10 12:38:34 -0500

    finalish part 1

--- hw5/hw5_team1.m
+++ hw5/hw5_team1.m
@@ -2,14 +2,14 @@ function hw5(serPort)
 
     global port;    
     port = serPort;
-    FWD_VEL = 0.1;
+    FWD_VEL = 0.07;
     FWD_DIST = 0.01;
-    ANGLE_VEL = 0.03;
+    ANGLE_VEL = 0.11;
     TURN_DEGREE = 1;
 
     % Reading your image
 %     image = imread('http://192.168.1.100/snapshot.cgi?user=admin&pwd=&resolution=16&rate=0');
-    image = imread('http://192.168.1.101/img/snapshot.cgi?');
+    image = imread('http://192.168.1.102/img/snapshot.cgi?');
     
     resolution = size(image); 
 	resolution = resolution(1:2);
@@ -28,10 +28,12 @@ function hw5(serPort)
     goal_area = area
     prev_area = area;
     prev_x = center;
+    speed = 0;
+    turn = 0;
     
     while(1)
 %         image = imread('http://192.168.1.100/snapshot.cgi?user=admin&pwd=&resolution=16&rate=0');
-        image = imread('http://192.168.1.101/img/snapshot.cgi?');
+        image = imread('http://192.168.1.102/img/snapshot.cgi?');
         pixel_mask = create_mask(image, rgb);
         [x, y, area, box] = find_largest_blob(pixel_mask);
         figure(1);
@@ -44,25 +46,52 @@ function hw5(serPort)
             continue;
         end
         
+        
         %movement stuff
-        if (x < .8*center) && (prev_x < .8*center)
+%         if (x < .8*center) && (prev_x < .8*center)
+%             fprintf('Turn left\n');
+%             turnAngle(port,ANGLE_VEL,TURN_DEGREE);
+%             continue;
+%         elseif (x > 1.2*center) && (prev_x > 1.2*center)
+%             fprintf('Turn right\n');
+%             turnAngle(port,ANGLE_VEL,-TURN_DEGREE);
+%             continue;
+%         end
+%         
+%         if (area < goal_area*.8) && (prev_area < goal_area*.8)
+%             fprintf('Move forward\n');
+%             travelDist(port, FWD_VEL, FWD_DIST);
+%         elseif (area > goal_area*1.2) && (prev_area > goal_area *1.2)
+%             fprintf('Move backward\n');
+%             travelDist(port, FWD_VEL, -FWD_DIST);
+%         end
+%       
+        
+        if (x > .8*center) && (x < 1.2*center)
+            turn = 0;
+        elseif (x < .8*center) && (prev_x < .8*center)
             fprintf('Turn left\n');
-            turnAngle(port,ANGLE_VEL,TURN_DEGREE);
-            continue;
+%             turnAngle(port,ANGLE_VEL,TURN_DEGREE);
+            turn = ANGLE_VEL;
         elseif (x > 1.2*center) && (prev_x > 1.2*center)
             fprintf('Turn right\n');
-            turnAngle(port,ANGLE_VEL,-TURN_DEGREE);
-            continue;
+%             turnAngle(port,ANGLE_VEL,-TURN_DEGREE);
+            turn = -ANGLE_VEL;
         end
         
-        if (area < goal_area*.8) && (prev_area < goal_area*.8)
+        if (area > goal_area*.8) && (area < goal_area*1.2)
+            speed = 0;
+        elseif (area < goal_area*.8) && (prev_area < goal_area*.8)
             fprintf('Move forward\n');
-            travelDist(port, FWD_VEL, FWD_DIST);
+%             travelDist(port, FWD_VEL, FWD_DIST);
+            speed = FWD_VEL;
         elseif (area > goal_area*1.2) && (prev_area > goal_area *1.2)
             fprintf('Move backward\n');
-            travelDist(port, FWD_VEL, -FWD_DIST);
+%             travelDist(port, FWD_VEL, -FWD_DIST);
+            speed = -FWD_VEL;
         end
         
+        SetFwdVelAngVelCreate(port, speed, turn);
         
         prev_x = x;
         prev_area = area;
